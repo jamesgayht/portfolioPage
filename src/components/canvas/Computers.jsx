acc77d41d03ab5ea58ffc9c2efd8fe193a055a0c
James Gay <jamesgayht@gmail.com>
2023-05-31 21:25:27 +0800

    completed portfolio webpage template

---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,83 @@
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
+
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
+  
+  console.info(">>> isMobile prop:", isMobile);
+
+  return (
+    // creating three.js elements with start with mesh not div
+    <mesh>
+      {/* need to add light if not we won't be able to see anything */}
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      {/*TODO: isMobile toggle not working */}
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.70 : 0.75}
+        position={isMobile ? [0, -3, 2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
+  );
+};
+
+const ComputerCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  // sets the isMobile variable
+  useEffect(() => {
+    // check whether is mobile device by matching max-width
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // set the initial state of isMobile
+    setIsMobile(mediaQuery.matches);
+
+    // handle mobile changes
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // remove event listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          // prevents up and down (y-axis) rotation
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputerCanvas;
---
+++ src/components/canvas/Earth.jsx
@@ -0,0 +1,49 @@
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
+
+const Earth = () => {
+  // useGLTF hook to import animation model
+  const earth = useGLTF("./planet/scene.gltf");
+
+  return (
+    <primitive
+      object={earth.scene}
+      scale={2.5}
+      position-y={0}
+      rotation-y={0}
+    />
+  );
+};
+
+const EarthCanvas = () => {
+  return (
+    <Canvas
+      shadows
+      frameloop="demand"
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6]
+      }}
+    >
+      {/* suspense makes sure something shows while the model is loading */}
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+
+        <Earth />
+      </Suspense>
+    </Canvas>
+  );
+};
+
+export default EarthCanvas;
